8f0b6ac9a8d64bf96c1adbfd44c3dbfc0a3a2033
Raksha Jaiswal <ku.rakshadilipjaiswal@gmail.com>
2024-02-04 20:40:35 +0530

    guard

---
+++ Office Assisment Full Stack/Angular/website/src/app/guard/role.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      
+
+      if(localStorage.getItem('role') === 'seller'){
+          return true;
+      }else{
+        alert("You can't access this");
+          return false;
+          
+      }
+
+  }
+  
+}
